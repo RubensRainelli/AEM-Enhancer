acfe612df134dadd1d9b5968c87d299738a59e2b
Rubens Rainelli <rubens@rainelli.it>
2023-05-10 19:06:20 +0200

    new file:   RubensRainelli.jpg 	modified:   background.js 	modified:   dist/bundle.js 	modified:   management.html 	modified:   manifest.json 	new file:   menu.json 	new file:   readme.MD

--- background.js
+++ background.js
@@ -7,42 +7,58 @@ chrome.runtime.onInstalled.addListener(function () {
 
   chrome.contextMenus.create({
     parentId: "aemEnhancer",
-    id: "openInEditor",
+    id: "openSelectedInEditor",
     title: "Open Selection in Editor",
     contexts: ["selection"],
   });
 
   chrome.contextMenus.create({
     parentId: "aemEnhancer",
-    id: "openInProperties",
+    id: "openSelectedInProperties",
     title: "Open Selection in Properties",
     contexts: ["selection"],
   });
 
-/*  chrome.contextMenus.create({
-    parentId: "aemEnhancer",
-    type: "separator",
-    title: "-----",
-    contexts: ["selection"],
-  });
-*/
   chrome.contextMenus.create({
     parentId: "aemEnhancer",
-    id: "openInAssets",
+    id: "openSelectedInPreview",
+    title: "Open Selection in Preview",
+    contexts: ["selection"],
+  });
+
+  chrome.contextMenus.create({
+    parentId: "aemEnhancer",
+    id: "openSelectedInAssets",
     title: "Open Selection in Assets",
     contexts: ["selection"],
   });
 
   chrome.contextMenus.create({
     parentId: "aemEnhancer",
+    id: "separator",
+    type: "separator",
+    title: "-----",
+    contexts: ["selection"],
+  });
+  
+  chrome.contextMenus.create({
+    parentId: "aemEnhancer",
     id: "copyRealPath",
     title: "Copy Real Path",
+    contexts: ["page"],
+  });
+/*
+  chrome.contextMenus.create({
+    parentId: "aemEnhancer",
+    id: "realPathManagePubblication",
+    title: "Manage Pubblication for Real Path of This Page",
     contexts: ["all"],
   });
+*/
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  if (info.menuItemId === "openInEditor") {
+  if (info.menuItemId === "openSelectedInEditor") {
     var pathCopiata = info.selectionText;
     var url = new URL(tab.url);
     var destinazione = url.origin + "/editor.html" + pathCopiata + ".html";
@@ -50,7 +66,7 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
     chrome.tabs.create({ url: destinazione });
   }
 
-  if (info.menuItemId === "openInProperties") {
+  if (info.menuItemId === "openSelectedInProperties") {
     var pathCopiata = info.selectionText;
     var url = new URL(tab.url);
     var destinazione = url.origin + "/mnt/overlay/wcm/core/content/sites/properties.html?item=" + pathCopiata;
@@ -58,7 +74,23 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
     chrome.tabs.create({ url: destinazione });
   }
 
-  if (info.menuItemId === "openInAssets") {
+  if (info.menuItemId === "openSelectedInPreview") {
+    var pathCopiata = info.selectionText;
+    var url = new URL(tab.url);
+    var destinazione = url.origin + pathCopiata + ".html?wcmmode=disabled";
+
+    chrome.tabs.create({ url: destinazione });
+  }
+/*
+  if (info.menuItemId === "realPathManagePubblication") {
+    var pathCopiata = info.selectionText;
+    var url = new URL(tab.url);
+    var destinazione = url.origin + "/mnt/override/libs/wcm/core/content/common/managepublicationwizard.html?item=" + pathCopiata;
+
+    chrome.tabs.create({ url: destinazione });
+  }
+*/
+  if (info.menuItemId === "openSelectedInAssets") {
     var pathCopiata = info.selectionText;
     var url = new URL(tab.url);
     var destinazione = url.origin + "/assets.html" + pathCopiata + ".html";
@@ -85,4 +117,28 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
       `,
     });
   }
+// TODO: Fix this code
+  if (info.menuItemId === "realPathManagePubblication") {
+    chrome.tabs.executeScript(tab.id, {
+      code: `
+      var button = document.querySelector('.js-editor-PageInfo-closePopover');
+      if (button) {
+        var dataPath = button.getAttribute('data-path');
+        var itemIndex = dataPath.indexOf('?item=');
+        var value = itemIndex !== -1 ? dataPath.substring(itemIndex + 7) : dataPath;
+        var tempInput = document.createElement('input');
+        var url = new URL(tab.url);
+      
+        document.body.appendChild(tempInput);
+        tempInput.value = "/mnt/override/libs/wcm/core/content/common/managepublicationwizard.html?item=/" + value;
+        tempInput.select();
+        var destinazione = url.origin + "/mnt/override/libs/wcm/core/content/common/managepublicationwizard.html?item=/" + tempInput;
+        document.body.removeChild(tempInput);
+        chrome.tabs.create({ url: destinazione });
+      
+      }      
+      `,
+    });
+  }
+
 });
