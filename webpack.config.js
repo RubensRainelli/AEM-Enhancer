52bbfed149bdf569f72d2f245a6e4224e961371c
Rubens Rainelli <rubens@rainelli.it>
2023-05-17 12:10:37 +0200

    0.1.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./background.js",
+  entry: "./service_worker.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
